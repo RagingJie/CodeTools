45a7ac82f3b52e6d15cf2577018c0f85ae1c92ed
naruto <2045933252@qq.com>
2025-07-02 16:36:12 +0800

    万能小工具--【unicode编码】--实现

--- src/common/Constant.java
+++ src/common/Constant.java
@@ -28,40 +28,4 @@ public class Constant {
     // 主窗口按钮字体
     public static final Font MAIN_WINDOW_BUTTON_FONT = new Font("楷体", Font.BOLD, 26);
 
-    // unicode编码 -- 窗体宽度
-    public static final int UNICODE_WINDOW_WIDTH = 880;
-
-    // unicode编码 -- 窗体高度
-    public static final int UNICODE_WINDOW_HEIGHT = 450;
-
-    // unicode编码 -- 文本域字体
-    public static final Font UNICODE_WINDOW_TEXT_AREA_FONT = new Font("楷体", Font.BOLD, 30);
-
-    // unicode编码 -- 窗体按钮字体
-    public static final Font UNICODE_WINDOW_BUTTON_FONT = new Font("楷体", Font.BOLD, 26);
-
-    // unicode编码 -- 窗体按钮宽度
-    public static final int UNICODE_WINDOW_BUTTON_WIDTH = 230;
-
-    // unicode编码 -- 窗体按钮高度
-    public static final int UNICODE_WINDOW_BUTTON_HEIGHT = 50;
-
-    // unicode编码 -- 窗体按钮x坐标
-    public static final int UNICODE_WINDOW_BUTTON_X = 100;
-
-    // unicode编码 -- 窗体按钮y坐标
-    public static final int UNICODE_WINDOW_BUTTON_Y = 350;
-
-    // unicode编码 -- 窗体文本域x坐标
-    public static final int UNICODE_WINDOW_TEXT_AREA_X = 25;
-
-    // unicode编码 -- 窗体文本域y坐标
-    public static final int UNICODE_WINDOW_TEXT_AREA_Y = 15;
-
-    // unicode编码 -- 窗体文本域宽度
-    public static final int UNICODE_WINDOW_TEXT_AREA_WIDTH = 820;
-
-    // unicode编码 -- 窗体文本域高度
-    public static final int UNICODE_WINDOW_TEXT_AREA_HEIGHT = 140;
-
 }
--- src/windowView/UnicodeWindow.java
+++ src/windowView/UnicodeWindow.java
@@ -1,41 +1,139 @@
 package windowView;
 
-import common.Constant;
+import tools.UnicodeUtil;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * @description: unicode编码窗口
  */
 public class UnicodeWindow extends JFrame {
 
+    // unicode编码 -- 窗体宽度
+    private static final int UNICODE_WINDOW_WIDTH = 880;
+
+    // unicode编码 -- 窗体高度
+    private static final int UNICODE_WINDOW_HEIGHT = 450;
+
+    // unicode编码 -- 文本域字体
+    private static final Font UNICODE_WINDOW_TEXT_AREA_FONT = new Font("楷体", Font.BOLD, 30);
+
+    // unicode编码 -- 窗体按钮字体
+    private static final Font UNICODE_WINDOW_BUTTON_FONT = new Font("楷体", Font.BOLD, 24);
+
+    // unicode编码 -- 窗体按钮宽度
+    private static final int UNICODE_WINDOW_BUTTON_WIDTH = 230;
+
+    // unicode编码 -- 窗体按钮高度
+    private static final int UNICODE_WINDOW_BUTTON_HEIGHT = 50;
+
+    // unicode编码 -- 窗体按钮x坐标
+    private static final int UNICODE_WINDOW_BUTTON_X = 60;
+
+    // unicode编码 -- 窗体按钮y坐标
+    private static final int UNICODE_WINDOW_BUTTON_Y = 183;
+
+    // unicode编码 -- 窗体文本域x坐标
+    private static final int UNICODE_WINDOW_TEXT_AREA_X = 25;
+
+    // unicode编码 -- 窗体文本域y坐标
+    private static final int UNICODE_WINDOW_TEXT_AREA_Y = 18;
+
+    // unicode编码 -- 窗体文本域宽度
+    private static final int UNICODE_WINDOW_TEXT_AREA_WIDTH = 820;
+
+    // unicode编码 -- 窗体文本域高度
+    private static final int UNICODE_WINDOW_TEXT_AREA_HEIGHT = 140;
+
+    // 输入文本
+    private static JTextArea inputText;
+    // 输出文本
+    private static JTextArea outputText;
+
     public UnicodeWindow() {
-        System.out.println("创建Unicode编码窗口");
         // 初始化窗体
         initWindow();
         // 创建窗体内容面板
         JPanel panel = CommonPanel.createjPanel();
         // 创建文本域
-        createTextArea(panel, 1);
+        inputText = createTextArea(panel, 1, true, new Color(0xEEEEEEEE, true));
+        outputText = createTextArea(panel, 2, false, new Color(0xEAE8E8E8, true));
+        // 创建按钮
+        createJButton("中文转换Unicode", panel, 1, 1);
+        createJButton("Unicode转换中文", panel, 2, 2);
+        createJButton("复制结果", panel, 3, 3);
         // 添加窗体内容面板
         add(panel);
     }
 
-    private void createTextArea(JPanel panel, int index) {
+    private JButton createJButton(String title, JPanel panel, int index, int type) {
+        // 创建按钮
+        JButton button = new JButton(title);
+        // 设置按钮字体
+        button.setFont(UNICODE_WINDOW_BUTTON_FONT);
+        // 设置按钮位置
+        button.setBounds(getButtonX(index), UNICODE_WINDOW_BUTTON_Y, UNICODE_WINDOW_BUTTON_WIDTH, UNICODE_WINDOW_BUTTON_HEIGHT);
+        // 添加按钮点击事件
+        button.addActionListener(e -> {
+            switch (type) {
+                case 1:
+                    outputText.setText(UnicodeUtil.toUnicodeEscape(inputText.getText()));
+                    break;
+                case 2:
+                    outputText.setText(UnicodeUtil.parseUnicodeEscapes(inputText.getText()));
+                    break;
+                case 3:
+                    // 复制结果
+                    outputText.selectAll();
+                    outputText.copy();
+                    break;
+            }
+        });
+        // 添加按钮
+        panel.add(button);
+        return button;
+    }
+
+    private int getButtonX(int index) {
+        if (index == 1)
+            return UNICODE_WINDOW_BUTTON_X;
+        else return UNICODE_WINDOW_BUTTON_X + (index - 1) * (UNICODE_WINDOW_BUTTON_WIDTH + 30);
+    }
+
+    private JTextArea createTextArea(JPanel panel, int index, boolean isEditable, Color backgroundColor) {
         // 创建文本域
         JTextArea textArea = new JTextArea();
         // 设置文本域不可编辑
-        textArea.setEditable(true);
+        textArea.setEditable(isEditable);
         // 设置文本域自动换行
         textArea.setLineWrap(true);
         // 设置文本域字体
-        textArea.setFont(Constant.UNICODE_WINDOW_TEXT_AREA_FONT);
+        textArea.setFont(UNICODE_WINDOW_TEXT_AREA_FONT);
+        // 设置文本域背景颜色
+        textArea.setBackground(backgroundColor);
         // 带滚动条的面板,把多行文本框组件加进来
         JScrollPane jsp = new JScrollPane(textArea);
         // 设置滚动条位置
-        jsp.setBounds(25, 15, 820, 140);
+        jsp.setBounds(UNICODE_WINDOW_TEXT_AREA_X, getTextAreaY(index), UNICODE_WINDOW_TEXT_AREA_WIDTH, UNICODE_WINDOW_TEXT_AREA_HEIGHT);
         // 添加文本域
         panel.add(jsp);
+        // 返回文本域
+        return textArea;
+    }
+
+    /**
+     * 获取文本域y坐标
+     *
+     * @param index
+     * @return
+     */
+    private int getTextAreaY(int index) {
+        if (index == 1) {
+            return UNICODE_WINDOW_TEXT_AREA_Y;
+        } else {
+            return UNICODE_WINDOW_TEXT_AREA_Y + (index - 1) * (UNICODE_WINDOW_TEXT_AREA_HEIGHT + UNICODE_WINDOW_BUTTON_HEIGHT + 50);
+        }
     }
 
     /**
@@ -47,7 +145,7 @@ public class UnicodeWindow extends JFrame {
         // 设置窗体关闭方式 -- 仅关闭当前窗口
         setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         // 设置窗体大小
-        setSize(Constant.UNICODE_WINDOW_WIDTH, Constant.UNICODE_WINDOW_HEIGHT);
+        setSize(UNICODE_WINDOW_WIDTH, UNICODE_WINDOW_HEIGHT);
         // 设置窗体居中
         setLocationRelativeTo(null);
         // 设置禁止窗口设置拖拽调整大小
